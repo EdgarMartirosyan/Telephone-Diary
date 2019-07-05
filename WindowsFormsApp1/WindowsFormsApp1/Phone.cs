291e1e61079cd4139cc5822bc7f51d17811c3beb
Edgar <edgar.martirosyan.a@gmail.com>
2019-07-05 18:07:52 +0400

    0507_1807

--- WindowsFormsApp1/WindowsFormsApp1/Phone.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Phone.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class Phone : Form
     {
+
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Phone;Integrated Security=True");
+
+
+
         public Phone()
         {
             InitializeComponent();
@@ -29,6 +35,119 @@ namespace WindowsFormsApp1
 
         private void Phone_Load(object sender, EventArgs e)
         {
+            Display();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox5.Clear();
+            comboBox1.SelectedIndex = -1;
+            textBox1.Focus();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(
+               @"INSERT INTO Mobilee (Frist, Last, Mobile,Emial,Category)
+               VALUES ('" + textBox1.Text + "','" + textBox2.Text + "' ,'" + textBox3.Text + "' ,'" + textBox5.Text + "' ,'" + comboBox1.Text + "' )", con
+                );
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfull Saved!");
+            Display();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(
+               @"DELETE FROM Mobilee
+               WHERE (Mobile='" + textBox3.Text + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfull deleted!");
+            Display();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        void Display()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("Select * from Mobilee", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = item[0].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item[1].ToString();
+                dataGridView1.Rows[n].Cells[2].Value = item[2].ToString();
+                dataGridView1.Rows[n].Cells[3].Value = item[3].ToString();
+                dataGridView1.Rows[n].Cells[4].Value = item[4].ToString();
+            }
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            textBox5.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            comboBox1.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(
+               @"UPDATE Mobilee
+                
+             SET    Frist='" + textBox1.Text + "',Last='" + textBox2.Text + "' ,Mobile='" + textBox3.Text + "' , Emial='" + textBox5.Text + "' ,Category='" + comboBox1.Text + "'  WHERE (Mobile='" + textBox3.Text + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Update Successfull!");
+            Display();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("Select * from Mobilee Where (Mobile like '%"+textBox4.Text+ "%') or (Frist like '%" + textBox4.Text + "%')", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = item[0].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item[1].ToString();
+                dataGridView1.Rows[n].Cells[2].Value = item[2].ToString();
+                dataGridView1.Rows[n].Cells[3].Value = item[3].ToString();
+                dataGridView1.Rows[n].Cells[4].Value = item[4].ToString();
+            }
 
         }
     }
